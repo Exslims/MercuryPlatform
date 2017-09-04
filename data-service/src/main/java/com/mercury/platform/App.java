4ba4bdae0dbc1007ffc339762bce8e021b07bc53
Exslims <exslims1@mail.ru>
2017-09-05 01:13:54 +0400

    Project skeleton with helloworld

---
+++ data-service/src/main/java/com/mercury/platform/App.java
@@ -0,0 +1,31 @@
+package com.mercury.platform;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//http://justincalleja.com/2016/04/17/serving-a-webpack-bundle-in-spring-boot/
+@EnableWebMvc
+@SpringBootApplication
+public class App implements ApplicationListener<ContextRefreshedEvent>{
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    @Value("${spring.profiles.active}")
+    protected String springProfilesActive;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        LOG.info("=======================================");
+        LOG.info("App running with active profiles: {}", springProfilesActive);
+        LOG.info("=======================================");
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
